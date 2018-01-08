8062ff657cd48c9681752928d3ab7f5dee3ff2a5
Pedro <pedroadamas@gmail.com>
2018-01-08 23:18:55 +0100

    edited organization of crypto files

--- crypto_analysis/crypto_data.py
+++ crypto_analysis/crypto_data.py
@@ -33,33 +33,6 @@ def get_quandl_data(quandl_id):
     return df
 
 
-btc_usd_price_kraken = get_quandl_data('BCHARTS/KRAKENUSD')
-
-
-btc_usd_price_kraken.head()
-
-# Chart the BTC pricing data
-# btc_trace = go.Scatter(x=btc_usd_price_kraken.index, y=btc_usd_price_kraken['Weighted Price'])
-# py.iplot([btc_trace])
-
-
-btc_usd_price_kraken['Weighted Price'].plot()
-plt.show()
-
-
-# Pull pricing data for 3 more BTC exchanges
-exchanges = ['COINBASE','BITSTAMP','ITBIT']
-
-exchange_data = {}
-
-exchange_data['KRAKEN'] = btc_usd_price_kraken
-
-for exchange in exchanges:
-    exchange_code = 'BCHARTS/{}USD'.format(exchange)
-    btc_exchange_df = get_quandl_data(exchange_code)
-    exchange_data[exchange] = btc_exchange_df
-
-
 def merge_dfs_on_column(dataframes, labels, col):
     '''Merge a single column of each dataframe into a new combined dataframe'''
     series_dict = {}
@@ -67,19 +40,6 @@ def merge_dfs_on_column(dataframes, labels, col):
         series_dict[labels[index]] = dataframes[index][col]
         
     return pd.DataFrame(series_dict)
-
-# Merge the BTC price dataseries' into a single dataframe
-btc_usd_datasets = merge_dfs_on_column(list(exchange_data.values()), list(exchange_data.keys()), 'Weighted Price')
-
-# Remove "0" values
-btc_usd_datasets.replace(0, np.nan, inplace=True)
-
-# Calculate the average BTC price as a new column
-btc_usd_datasets['avg_btc_price_usd'] = btc_usd_datasets.mean(axis=1)
-
-btc_usd_datasets['avg_btc_price_usd'].plot()
-
-plt.show()
 
 
 ###############
@@ -97,40 +57,3 @@ def get_json_data(json_url, cache_path):
         df.to_pickle(cache_path)
         print('Cached {} at {}'.format(json_url, cache_path))
     return df
-
-base_polo_url = 'https://poloniex.com/public?command=returnChartData&currencyPair={}&start={}&end={}&period={}'
-start_date = datetime.strptime('2015-01-01', '%Y-%m-%d') # get data from the start of 2015
-end_date = datetime.now() # up until today
-pediod = 86400 # pull daily data (86,400 seconds per day)
-
-def get_crypto_data(poloniex_pair):
-    '''Retrieve cryptocurrency data from poloniex'''
-    json_url = base_polo_url.format(poloniex_pair, start_date.timestamp(), end_date.timestamp(), pediod)
-    data_df = get_json_data(json_url, poloniex_pair)
-    data_df = data_df.set_index('date')
-    return data_df
-
-
-altcoins = ['ETH','LTC','XRP','ETC','STR','DASH','SC','XMR','XEM']
-
-altcoin_data = {}
-for altcoin in altcoins:
-    coinpair = 'BTC_{}'.format(altcoin)
-    crypto_price_df = get_crypto_data(coinpair)
-    altcoin_data[altcoin] = crypto_price_df
-
-# Calculate USD Price as a new column in each altcoin dataframe
-for altcoin in altcoin_data.keys():
-    altcoin_data[altcoin]['price_usd'] =  altcoin_data[altcoin]['weightedAverage'] * btc_usd_datasets['avg_btc_price_usd']
-
-# Merge USD price of each altcoin into single dataframe 
-combined_df = merge_dfs_on_column(list(altcoin_data.values()), list(altcoin_data.keys()), 'price_usd')
-
-# Add BTC price to the dataframe
-combined_df['BTC'] = btc_usd_datasets['avg_btc_price_usd']
-
-print(combined_df)
-
-combined_df.plot()
-
-plt.show()
