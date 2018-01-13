47fb96b5948f351c2ee4324463d5e6e320bc9697
Pedro <pedroadamas@gmail.com>
2018-01-13 20:50:28 +0100

    now it saves the data at each iteration

--- finances/porfolio/portfolio.py
+++ finances/porfolio/portfolio.py
@@ -1,7 +1,11 @@
 import pandas as pd
+from datetime import datetime
+import pickle
+import os
 
 import crypto_currencies.coin_marketcap as market
 
+cfd, cfn = os.path.split(os.path.abspath(__file__))
 
 class PortFolio():
 	assets = {}
@@ -9,17 +13,36 @@ class PortFolio():
 	transactions = pd.DataFrame()
 	value = 0
 	invested = 0
+	portfolio_location = os.path.join(cfd, 'my_portfolio.pk')
 
 	def __init__(self, assets, invested):
 		self.assets = assets
 		self.invested = invested
+		f = open(self.portfolio_location, 'rb')
+		self.historical_value = pickle.load(f)
 
 	def update_value(self):
 		value = 0
 		for asset in self.assets:
 			asset_value= market.get_coin_value(asset)
 			value += self.assets[asset]*asset_value
+			print(asset, asset_value, self.assets[asset]*asset_value)
 		self.value = value
+
+	def save_portfolio_prices(self):
+		pd.DataFrame(index=[datetime.now()])
+		temp_df = pd.DataFrame(index=[datetime.now()])
+
+		for asset in self.assets:
+			temp_df[asset] = market.get_coin_value(asset)
+
+		self.historical_value = self.historical_value.append(temp_df)
+		print(self.historical_value)
+		self.historical_value.to_pickle(self.portfolio_location)
+		self.historical_value.to_csv(os.path.join(cfd, 'my_portfolio.csv'))
+
+
+
 
 
 
@@ -45,3 +68,8 @@ if __name__=='__main__':
 	myportfolio = PortFolio(assets = portfolio_assets, invested = 2270.0)
 	myportfolio.update_value()
 	print(myportfolio.value)
+
+	myportfolio.save_portfolio_prices()
+
+	from pprint import pprint
+	pprint(myportfolio)
