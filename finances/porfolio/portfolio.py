adc30c9396dc0f670c532d037e07fe88dbe72cd5
Pedro <pedroadamas@gmail.com>
2018-01-13 17:41:36 +0100

    update. Working, but with limited number of currencies

--- finances/porfolio/portfolio.py
+++ finances/porfolio/portfolio.py
@@ -4,7 +4,6 @@ from crypto_currencies import crypto_data
 
 
 class PortFolio():
-
 	assets = {}
 	historical_value = pd.DataFrame()
 	transactions = pd.DataFrame()
@@ -16,7 +15,8 @@ class PortFolio():
 	def update_value(self):
 		value = 0
 		for asset in self.assets:
-			asset_value = crypto_data.get_btc_price('Close')[-1]
+			crypto_price_df = crypto_data.get_crypto_vs_usd_data(asset)
+			asset_value = crypto_price_df.dropna().close[-1]
 			value += self.assets[asset]*asset_value
 		self.value = value
 
@@ -25,6 +25,7 @@ class PortFolio():
 
 
 if __name__=='__main__':
-	a = PortFolio({'BTC':0.5, 'ETH':0.5})
+	a = PortFolio({'BTC':0.5, 'ETH':0.5, 'XLM':1001})
 	a.update_value()
 	print(a.value)
+	# print(crypto_data.get_crypto_vs_usd_data('XRP'))
