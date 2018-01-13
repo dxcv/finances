8ba5179cb1c9db8e423fd988cc39879adff5d8c1
Pedro <pedroadamas@gmail.com>
2018-01-13 18:19:12 +0100

    first version of portfolio value update made

--- finances/porfolio/portfolio.py
+++ finances/porfolio/portfolio.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from crypto_currencies import crypto_data
+import crypto_currencies.coin_marketcap as market
 
 
 class PortFolio():
@@ -8,15 +8,16 @@ class PortFolio():
 	historical_value = pd.DataFrame()
 	transactions = pd.DataFrame()
 	value = 0
+	invested = 0
 
-	def __init__(self, assets):
+	def __init__(self, assets, invested):
 		self.assets = assets
+		self.invested = invested
 
 	def update_value(self):
 		value = 0
 		for asset in self.assets:
-			crypto_price_df = crypto_data.get_crypto_vs_usd_data(asset)
-			asset_value = crypto_price_df.dropna().close[-1]
+			asset_value= market.get_coin_value(asset)
 			value += self.assets[asset]*asset_value
 		self.value = value
 
@@ -25,7 +26,22 @@ class PortFolio():
 
 
 if __name__=='__main__':
-	a = PortFolio({'BTC':0.5, 'ETH':0.5, 'XLM':1001})
-	a.update_value()
-	print(a.value)
-	# print(crypto_data.get_crypto_vs_usd_data('XRP'))
+	portfolio_assets = {
+		'BTC': 0.007,
+		'ETH': 2.14081,
+		'XRP': 922.5,
+		'ADA': 926,
+		'XLM': 929.07,
+		'LTC': 1.0,
+		'TRX': 2760,
+		'UBQ': 18.222,
+		'BIS': 36.6,
+		'IOTA': 47.553,
+		'EMC2': 45,
+		'FUN': 633.366,
+		'ADST': 136.71
+	}
+
+	myportfolio = PortFolio(assets = portfolio_assets, invested = 2270.0)
+	myportfolio.update_value()
+	print(myportfolio.value)
