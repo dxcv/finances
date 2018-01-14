ff935ffbd04d3bce4e661b859af2ae6a14aff16d
Pedro <pedroadamas@gmail.com>
2018-01-14 17:43:44 +0100

    updated with loading files from folder

--- finances/porfolio/portfolio.py
+++ finances/porfolio/portfolio.py
@@ -8,18 +8,29 @@ import crypto_currencies.coin_marketcap as market
 cfd, cfn = os.path.split(os.path.abspath(__file__))
 
 class PortFolio():
+	name = 'MyPortfolio'
 	assets = {}
-	historical_value = pd.DataFrame()
-	transactions = pd.DataFrame()
 	value = 0
-	invested = 0
-	portfolio_location = os.path.join(cfd, 'my_portfolio.pk')
+	invest = 0
+	prices_history = pd.DataFrame()
+	assets_history = pd.DataFrame()
+	value_history = pd.DataFrame()
 
-	def __init__(self, assets, invested):
+	def __init__(self, name, assets, invested):
 		self.assets = assets
 		self.invested = invested
-		f = open(self.portfolio_location, 'rb')
-		self.historical_value = pickle.load(f)
+		self.name = name
+
+
+	def set_data_from_path(self, path=os.path.join(cfd)):
+		f = open(os.path.join(path, self.name, 'prices_history.pk'), 'rb')
+		self.prices_history = pickle.load(f)
+
+		f = open(os.path.join(path, self.name, 'assets_history.pk'), 'rb')
+		self.assets_history = pickle.load(f)
+
+		self.portfolio_path = os.path.join(path, self.name)
+
 
 	def update_value(self):
 		value = 0
@@ -29,19 +40,31 @@ class PortFolio():
 			print(asset, asset_value, self.assets[asset]*asset_value)
 		self.value = value
 
-	def add_current_price(self):
-		pd.DataFrame(index=[datetime.now()])
-		temp_df = pd.DataFrame(index=[datetime.now()])
+	def update_prices_history(self, name='prices_history'):
+		temp_df = pd.DataFrame(index=[datetime.now().replace(second=0, microsecond=0)])
 
 		for asset in self.assets:
 			temp_df[asset] = market.get_coin_value(asset)
 
-		self.historical_value = self.historical_value.append(temp_df)
-		print(self.historical_value)
-		self.historical_value.to_pickle(self.portfolio_location)
-		self.historical_value.to_csv(os.path.join(cfd, 'my_portfolio.csv'))
+		self.prices_history = self.prices_history.append(temp_df)
+		self.prices_history.to_pickle(os.path.join(self.portfolio_path, name+'.pk'))
+		self.prices_history.to_csv(os.path.join(self.portfolio_path, name+'.csv'))
 
+	def update_assets_history(self, name='assets_history'):
+		temp_df = pd.DataFrame(self.assets, index=[datetime.now().replace(second=0, microsecond=0)])
+		self.assets_history = self.assets_history.append(temp_df)
 
+		self.assets_history.to_pickle(os.path.join(self.portfolio_path, name+'.pk'))
+		self.assets_history.to_csv(os.path.join(self.portfolio_path, name+'.csv'))
+
+	def update_portfolio_history(self, assets_name, prices_name):
+		self.update_prices_history(name=prices_name)
+		self.update_assets_history(name=assets_name)
+
+	def  create_value_dataframe(self):
+		for asset in self.assets_history.columns:
+			self.value_history[asset] = self.prices_history[asset]*self.assets_history[asset]
+		return self.value_history
 
 
 
@@ -68,19 +91,16 @@ if __name__=='__main__':
 	import pylab as plt
 	from pprint import pprint
 	
-	myportfolio = PortFolio(assets = portfolio_assets, invested = 2270.0)
-	myportfolio.update_value()
-	print(myportfolio.value)
+	myportfolio = PortFolio(name= 'MyPortfolio', assets = portfolio_assets, invested = 2270.0)
+	myportfolio.set_data_from_path(path=os.path.join(cfd))
 
-	# myportfolio.add_current_price()
-	print(myportfolio.historical_value)
+	# myportfolio.update_portfolio_history(assets_name='assets_history', prices_name='prices_history')
+	print(myportfolio.assets_history)
+	print(myportfolio.prices_history)
 
-	values = myportfolio.historical_value
-	for key in values.columns:
-		values[key]=values[key]*myportfolio.assets[key]
-
-	values['Total_value'] = values.sum(axis=1)
-	print(values)
-	values.plot()
+	print('#################################################################')
+	values_df = myportfolio.create_value_dataframe()
+	values_df.plot()
 	plt.show()
+
 
