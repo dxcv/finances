588dc0fb95f7c05104e93e522b97f784effe74f7
Pedro <pedroadamas@gmail.com>
2018-01-27 11:54:49 +0100

    first working file with potfolioopt markowitz optimization

--- finances/porfolio/portfolio_optimization.py
+++ finances/porfolio/portfolio_optimization.py
@@ -2,6 +2,23 @@ import pandas as pd
 import datetime
 import pickle
 import os
-import portfolioopt
+import portfolioopt as pfopt
+
+from market import market_data as mkt_data
 
 cfd, cfn = os.path.split(os.path.abspath(__file__))
+
+mkt=mkt_data.MarketData()
+returns = mkt.crypto_returns_history(symbols=['BTC', 'ETH', 'XRP', 'XLM', 'ADA', 'TRX', 'ADST']).dropna()
+
+avg_rets = returns.mean()
+cov_mat = returns.cov()
+
+print(avg_rets)
+
+a= avg_rets.quantile(0.5)
+
+weights = pfopt.markowitz_portfolio(cov_mat=cov_mat, exp_rets=avg_rets, target_ret=a)
+
+print(weights)
+print(weights.values)
