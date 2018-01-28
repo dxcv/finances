af549baf806a5140f9b1ba53dfd926f6dc26b066
Pedro <pedroadamas@gmail.com>
2018-01-28 18:39:36 +0100

    working portfolio optimization

--- finances/porfolio/portfolio_optimization.py
+++ finances/porfolio/portfolio_optimization.py
@@ -9,16 +9,23 @@ from market import market_data as mkt_data
 cfd, cfn = os.path.split(os.path.abspath(__file__))
 
 mkt=mkt_data.MarketData()
-returns = mkt.crypto_returns_history(symbols=['BTC', 'ETH', 'XRP', 'XLM', 'ADA', 'TRX', 'ADST']).dropna()
+returns = mkt.crypto_returns_history(symbols=['BTC', 'ETH', 'XRP', 'XLM', 'ADA', 'TRX', 'ADST', 'BIS', 'IOTA']).dropna()
 
 avg_rets = returns.mean()
 cov_mat = returns.cov()
 
-print(avg_rets)
+print(cov_mat)
 
-a= avg_rets.quantile(0.5)
+tgt_return = avg_rets.quantile(0.6)
 
-weights = pfopt.markowitz_portfolio(cov_mat=cov_mat, exp_rets=avg_rets, target_ret=a)
+print(tgt_return)
 
-print(weights)
-print(weights.values)
+weights_optimal = pfopt.markowitz_portfolio(cov_mat=cov_mat, exp_rets=avg_rets, target_ret=tgt_return)
+weights_optimal = pfopt.truncate_weights(weights_optimal)
+
+print(weights_optimal)
+
+weights_sharpe = pfopt.tangency_portfolio(cov_mat=cov_mat, exp_rets=avg_rets)
+weights_sharpe = pfopt.truncate_weights(weights_sharpe)
+
+print(weights_sharpe)
