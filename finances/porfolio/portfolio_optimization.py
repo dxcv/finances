a22daca13ce240363a0c6e44283f953a2e9a5b44
Pedro <pedroadamas@gmail.com>
2018-02-02 23:54:56 +0100

    first working file with the calculation of the markowitz frontier

--- finances/porfolio/portfolio_optimization.py
+++ finances/porfolio/portfolio_optimization.py
@@ -13,54 +13,52 @@ import statsmodels.api as sm
 cfd, cfn = os.path.split(os.path.abspath(__file__))
 
 mkt=mkt_data.MarketData()
-returns = mkt.crypto_returns_history(symbols=['ADA', 'ADST', 'BTC', 'BIS', 'NEO', 'EMC2', 'ETH', 'FUN', 'IOTA', 'LTC', 'TRX', 'UBQ', 'XLM', 'XRP']).dropna()
+returns = mkt.crypto_returns_history(
+    symbols=['ADA', 'XMR', 'ADST', 'BTC', 'BIS', 'NEO', 'EMC2', 'ETH', 'FUN', 'IOTA', 'LTC', 'TRX', 'UBQ', 'XLM', 'XRP']
+    ).dropna()
 
-print(returns)
-month_returns = pd.DataFrame()
+def generate_projected_normal_sample(returns_data, N=30, sample_size=1000):
+    projected_returns = pd.DataFrame()
+    for r in returns_data:
+        rets = returns_data[r]
+        
+        # MLE of the sample for a normal distribution (to be improved) 
+        mu, std = norm.fit(rets)
+        projected_mu, projected_std = N*mu, std*np.sqrt(N)
 
-# for r in returns.columns:
-#     print(r)
-#     rets = returns[r]
-#     kde = sm.nonparametric.KDEUnivariate(rets)
-#     kde.fit()
-#     ret_value = kde.support
-#     pdf = kde.density
-#     prob = pdf*(ret_value[-1]-ret_value[0])/(len(pdf)-1)
-#     prob[prob < 0] = 0
-#     sample = np.random.choice(ret_value, p=prob, size=100000)
-#     for k in range(1):
-#         sample += np.random.choice(ret_value, p=prob, size=100000)
-#     month_returns[r] = sample
+        # generate big sample with the projected distribution
+        projected_data = norm.rvs(projected_mu, projected_std, size=sample_size)
+        
+        #add that as new returns for the month
+        projected_returns[r] = projected_data
 
-n_days = 30
+    return projected_returns
 
-# calculate monthly distribution
-for r in returns.columns:
-    rets = returns[r]
-    daily_mu, daily_std = norm.fit(rets)
-    monthly_mu, monthly_std = n_days*daily_mu, daily_std*np.sqrt(n_days)
-    monthly_data = norm.rvs(monthly_mu, monthly_std, size=1000)
-    month_returns[r] = monthly_data
+def markowitz_efficient_frontier(returns_data):
+    avg_rets = returns_data.mean()
+    cov_mat = returns_data.cov()
 
+    # calculate the Markowitz optimal allocations for each target return value
+    optimal_allocation_list = []
+    for k in np.arange(0.01, 0.99, 0.01):
+        tgt_return = avg_rets.quantile(k)
+        optimal_weights = pfopt.markowitz_portfolio(cov_mat=cov_mat, exp_rets=avg_rets, target_ret=tgt_return)
+        optimal_allocation_list.append(optimal_weights)
 
-returns = month_returns
-print(returns)
+    matrix_rets = avg_rets.as_matrix()
+    matrix_cov = cov_mat.as_matrix()
+    returns = [x.dot(matrix_rets) for x in optimal_allocation_list]
+    risks = [np.sqrt(x.dot(matrix_cov.dot(x))) for x in optimal_allocation_list]
 
-avg_rets = returns.mean()
-cov_mat = returns.cov()
+    return returns, risks
 
-print(cov_mat)
-
-tgt_return = avg_rets.quantile(0.6)
-
-print(tgt_return)
-
-weights_optimal = pfopt.markowitz_portfolio(cov_mat=cov_mat, exp_rets=avg_rets, target_ret=tgt_return)
-weights_optimal = pfopt.truncate_weights(weights_optimal)
-
-print(weights_optimal)
-
-weights_sharpe = pfopt.tangency_portfolio(cov_mat=cov_mat, exp_rets=avg_rets)
-weights_sharpe = pfopt.truncate_weights(weights_sharpe)
-
-print(weights_sharpe)
+if __name__=='__main__':
+    import seaborn as sns
+    sns.set()
+    sns.set_palette('YlOrRd', 12)
+    for days in range(1,60, 5):
+        monthly_returns = generate_projected_normal_sample(returns, days)
+        rewards, risks = markowitz_efficient_frontier(monthly_returns)
+        plt.plot(risks, rewards, label='%i days' % days)
+    plt.legend()
+    plt.show()
