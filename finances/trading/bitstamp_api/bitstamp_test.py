1dcb1c840223f290c88f3dc80aa952b50af01e86
Pedro <pedroadamas@gmail.com>
2018-02-26 00:48:23 +0100

    some changes. Working

--- finances/trading/bitstamp_api/bitstamp_test.py
+++ finances/trading/bitstamp_api/bitstamp_test.py
@@ -11,7 +11,11 @@ import time
 import pandas as pd
 
 public_client = bitstamp.client.Public()
+
+import time
  
+# Wait for 5 seconds
+time.sleep(5)
  
 trading_client = bitstamp.client.Trading(
        username='769101',
@@ -19,13 +23,13 @@ trading_client = bitstamp.client.Trading(
        secret='m9DEG4zucVxMVp0vWyROpkJUBXKvkL0F'
        )
 
-print(trading_client.account_balance())
+# print(trading_client.account_balance())
  
  
-# for k in range(100000):
-#     dic_entry = trading_client.ticker_hour()  # Can access public methods
-#     for k in dic_entry:
-#         bit_dic[k].append(dic_entry[k])
-#     df = pd.DataFrame.from_dict(bit_dic)
-#     time.sleep(10)
-#     df.to_csv('first_test.csv')
+for k in range(100000):
+    print(k, trading_client.ticker(base='bch', quote='btc'))  # Can access public methods
+    # for k in dic_entry:
+    #     bit_dic[k].append(dic_entry[k])
+    # df = pd.DataFrame.from_dict(bit_dic)
+    time.sleep(1)
+    # df.to_csv('first_test.csv')
