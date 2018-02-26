2cd08fb36a5e730a72491999e7eec5e36f276e10
Pedro <pedroadamas@gmail.com>
2018-02-26 23:26:44 +0100

    bash file to run the test bot

---
+++ finances/python_anywhere/test_bot_script.sh
@@ -0,0 +1,5 @@
+cd /home/hordeus/finances/finances/
+
+git checkout update_data
+
+/home/hordeus/.virtualenvs/finances/bin/python3.6 /home/hordeus/finances/finances/trading/bitstamp_api/run_trading_public.py
