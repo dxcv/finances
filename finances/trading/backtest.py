eac2514bb64f9de9f1210f5961b3659c46f1c973
Pedro <pedroadamas@gmail.com>
2018-02-28 21:43:27 +0100

    edited to be more organized the backtest

--- finances/trading/backtest.py
+++ finances/trading/backtest.py
@@ -10,7 +10,15 @@ import statsmodels.api as sm
 
 from trading.strategies.stoploss import adv_stop_loss_strategy
 
-def back_test_random(price_data, n=200, time_delta_stress_test=datetime.timedelta(days=30)):
+pct_gap = 0.01
+
+def back_test_stop_loss_strategy(
+    price_data,
+    strategy=adv_stop_loss_strategy,
+    n=200,
+    time_delta_stress_test=datetime.timedelta(days=30),
+    view_result=False
+    ):
 
     compare_dic = {'hold':[], 'strategy':[], 'diff':[]}
     date_list=[]
@@ -24,21 +32,64 @@ def back_test_random(price_data, n=200, time_delta_stress_test=datetime.timedelt
 
         backtest_df['hold'] = prices_test_data*100.0/prices_test_data.iloc[0]
 
-        strategy, stop=adv_stop_loss_strategy(price_series=prices_test_data)
+        strategy_result, stop=strategy(price_series=prices_test_data, pct_gap = pct_gap, reinvest_gap=0.25)
 
-        backtest_df['strategy'] = strategy
+        backtest_df['strategy'] = strategy_result
 
         #
         backtest_df['stop'] = stop
         backtest_df['stop_+'] = stop*(1+pct_gap)
         backtest_df['stop_-'] = stop*(1-pct_gap)
 
-        # fig, ax = plt.subplots(2,1, sharex=True)
-        # backtest_df[['strategy', 'hold']].plot(ax=ax[0])
-        # prices_test_data.plot(ax=ax[1])
-        # backtest_df['stop'].plot(ax=ax[1], style='k')
-        # backtest_df['stop_+'].plot(ax=ax[1], style='g')
-        # backtest_df['stop_-'].plot(ax=ax[1], style='r')
+        if view_result:
+            if n>25:
+                raise('Sample too big. Reduce number of tests')
+            fig, ax = plt.subplots(2,1, sharex=True)
+            backtest_df[['strategy', 'hold']].plot(ax=ax[0])
+            prices_test_data.plot(ax=ax[1])
+            backtest_df['stop'].plot(ax=ax[1], style='k')
+            backtest_df['stop_+'].plot(ax=ax[1], style='g')
+            backtest_df['stop_-'].plot(ax=ax[1], style='r')
+
+        for t in ['hold', 'strategy']:
+            compare_dic[t].append(backtest_df[t].iloc[-1]-100.0)
+
+        compare_dic['diff'].append(backtest_df['strategy'].iloc[-1]-backtest_df['hold'].iloc[-1])
+
+    return pd.DataFrame(data=compare_dic, index=date_list)
+
+
+def back_test_random(
+    price_data,
+    strategy=adv_stop_loss_strategy,
+    n=200,
+    time_delta_stress_test=datetime.timedelta(days=30),
+    view_result=False
+    ):
+
+    compare_dic = {'hold':[], 'strategy':[], 'diff':[]}
+    date_list=[]
+    for k in range(n):
+        start_test = random.choice(price_data.index)
+        end_test=start_test+time_delta_stress_test
+        date_list.append(start_test)
+        
+        prices_test_data = price_data.loc[start_test:end_test]
+        backtest_df = pd.DataFrame()
+
+        backtest_df['hold'] = prices_test_data*100.0/prices_test_data.iloc[0]
+
+        strategy_result, stop=strategy(price_series=prices_test_data)
+
+        backtest_df['strategy'] = strategy_result
+
+
+        if view_result:
+            if n>25:
+                raise('Sample too big. Reduce number of tests')
+            fig, ax = plt.subplots(2,1, sharex=True)
+            backtest_df[['strategy', 'hold']].plot(ax=ax[0])
+            prices_test_data.plot(ax=ax[1])
 
         for t in ['hold', 'strategy']:
             compare_dic[t].append(backtest_df[t].iloc[-1]-100.0)
@@ -52,8 +103,8 @@ if __name__=='__main__':
 
     mkt=mkt_data.MarketData()
 
-    price_data = mkt.crypto_data['XLM'].dropna()
-    df = back_test_random(price_data)
+    price_data = mkt.crypto_data['XLM'].loc[datetime.datetime(2018,1,27):].dropna()
+    df = back_test_stop_loss_strategy(price_data, n=10, view_result=True)
     df.boxplot()
 
     for c in ['hold', 'strategy']:
