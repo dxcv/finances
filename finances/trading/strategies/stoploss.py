ac07b5033a3e190b2b63d8e50061673b008de07a
Pedro <pedroadamas@gmail.com>
2018-03-04 15:57:23 +0100

    some changes to introduce the decision step function

--- finances/trading/strategies/stoploss.py
+++ finances/trading/strategies/stoploss.py
@@ -13,7 +13,49 @@ cfd, cfn = os.path.split(os.path.abspath(__file__))
 
 pct_gap = 0.01
 
-def stop_loss_strategy(price_series, pct_gap=pct_gap, fee=0.0025, invested_value=100):
+def strategy_decision(
+    current_price,
+    stoploss_value,
+    cash,
+    coin_amount,
+    state,
+    reinvest_gap=0.2,
+    pct_gap=0.025,
+    fee=0.0025):
+
+    if cash==0 and coin_amount>0:
+        if current_price < stoploss_value*(1-pct_gap) and state == 0:
+            cash = coin_amount*current_price*(1-fee)
+            coin_amount = 0
+            stoploss_value=current_price
+
+        elif current_price < stoploss_value*(1+pct_gap) and state == 1:
+            cash = coin_amount*current_price*(1-fee)
+            coin_amount = 0
+
+
+        elif current_price>stoploss_value*(1+reinvest_gap) and state==1:
+            stoploss_value=stoploss_value*(1+reinvest_gap)
+
+    elif coin_amount==0 and cash>0:
+        if current_price > stoploss_value*(1+pct_gap) and state==0:
+            coin_amount = cash/(current_price)*(1-fee)
+            cash = 0
+            stoploss_value=current_price
+
+        elif current_price > stoploss_value*(1-pct_gap) and state == -1:
+            coin_amount = cash/(current_price)*(1-fee)
+            cash = 0
+
+        elif current_price<stoploss_value*(1-reinvest_gap) and state==-1:
+            coin_amount = cash/(stoploss_value*(1-reinvest_gap))*(1-fee)
+            cash = 0
+            stoploss_value=stoploss_value*(1-reinvest_gap)
+    
+    return cash, coin_amount, stoploss_value
+
+
+def ideal_adv_stop_loss_strategy(price_series, reinvest_gap=0.2, pct_gap=pct_gap, fee=0.0025, invested_value=100):
     stoploss_value = price_series.iloc[0]
 
     coin_amount = invested_value/price_series.iloc[0]
@@ -35,6 +77,9 @@ def stop_loss_strategy(price_series, pct_gap=pct_gap, fee=0.0025, invested_value
                 cash = coin_amount*stoploss_value*(1+pct_gap)*(1-fee)
                 coin_amount = 0
 
+            elif current_price>stoploss_value*(1+reinvest_gap) and state==1:
+                stoploss_value=stoploss_value*(1+reinvest_gap)
+
         elif coin_amount==0 and cash>0:
             if current_price > stoploss_value*(1+pct_gap) and state==0:
                 coin_amount = cash/(stoploss_value*(1+pct_gap))*(1-fee)
@@ -45,7 +90,50 @@ def stop_loss_strategy(price_series, pct_gap=pct_gap, fee=0.0025, invested_value
                 coin_amount = cash/(stoploss_value*(1-pct_gap))*(1-fee)
                 cash = 0
 
+            elif current_price<stoploss_value*(1-reinvest_gap) and state==-1:
+                coin_amount = cash/(stoploss_value*(1-reinvest_gap))*(1-fee)
+                cash = 0
+                stoploss_value=stoploss_value*(1-reinvest_gap)
+
         # check state
+
+        if current_price >= stoploss_value*(1+pct_gap):
+            state=1
+
+        elif current_price <= stoploss_value*(1-pct_gap):
+            state=-1
+
+        else:
+            state=0
+
+        value = coin_amount*current_price+cash
+        trade_value.append(value)
+        stoploss.append(stoploss_value)
+
+    return pd.Series(data=trade_value, index=price_series.index), pd.Series(data=stoploss, index=price_series.index)
+
+
+def stop_loss_strategy(price_series, pct_gap=pct_gap, fee=0.0025, invested_value=100):
+    stoploss_value = price_series.iloc[0]
+
+    coin_amount = invested_value/price_series.iloc[0]
+    cash = 0
+
+    trade_value = [invested_value]
+    stoploss=[stoploss_value]
+    state = 0
+    for k in range(1,len(price_series)):
+        current_price = price_series.iloc[k]
+
+        cash, coin_amount, stoploss_value = strategy_decision(
+            current_price=current_price,
+            cash=cash,
+            stoploss_value=stoploss_value,
+            coin_amount=coin_amount,
+            state=state,
+            reinvest_gap=reinvest_gap,
+            pct_gap=pct_gap,
+            fee=fee)
 
         if current_price >= stoploss_value*(1+pct_gap):
             state=1
@@ -75,33 +163,15 @@ def adv_stop_loss_strategy(price_series, reinvest_gap=0.2, pct_gap=pct_gap, fee=
     for k in range(1,len(price_series)):
         current_price = price_series.iloc[k]
 
-        if cash==0 and coin_amount>0:
-            if current_price < stoploss_value*(1-pct_gap) and state == 0:
-                cash = coin_amount*current_price*(1-fee)
-                coin_amount = 0
-                stoploss_value=current_price
-
-            elif current_price < stoploss_value*(1+pct_gap) and state == 1:
-                cash = coin_amount*current_price*(1-fee)
-                coin_amount = 0
-
-            elif current_price>stoploss_value*(1+reinvest_gap) and state==1:
-                stoploss_value=stoploss_value*(1+reinvest_gap)
-
-        elif coin_amount==0 and cash>0:
-            if current_price > stoploss_value*(1+pct_gap) and state==0:
-                coin_amount = cash/(current_price)*(1-fee)
-                cash = 0
-                stoploss_value=current_price
-
-            elif current_price > stoploss_value*(1-pct_gap) and state == -1:
-                coin_amount = cash/(current_price)*(1-fee)
-                cash = 0
-
-            elif current_price<stoploss_value*(1-reinvest_gap) and state==-1:
-                coin_amount = cash/(stoploss_value*(1-reinvest_gap))*(1-fee)
-                cash = 0
-                stoploss_value=stoploss_value*(1-reinvest_gap)
+        cash, coin_amount, stoploss_value = strategy_decision(
+            current_price=current_price,
+            cash=cash,
+            stoploss_value=stoploss_value,
+            coin_amount=coin_amount,
+            state=state,
+            reinvest_gap=reinvest_gap,
+            pct_gap=pct_gap,
+            fee=fee)
 
         # check state
 
@@ -121,74 +191,34 @@ def adv_stop_loss_strategy(price_series, reinvest_gap=0.2, pct_gap=pct_gap, fee=
     return pd.Series(data=trade_value, index=price_series.index), pd.Series(data=stoploss, index=price_series.index)
 
 
-def back_test_random(price_data, n=200, time_delta_stress_test=datetime.timedelta(days=30)):
-
-    compare_dic = {'hold':[], 'strategy':[], 'diff':[]}
-    date_list=[]
-    for k in range(n):
-        start_test = random.choice(price_data.index)
-        end_test=start_test+time_delta_stress_test
-        date_list.append(start_test)
-        
-        prices_test_data = price_data.loc[start_test:end_test]
-        backtest_df = pd.DataFrame()
-
-        backtest_df['hold'] = prices_test_data*100.0/prices_test_data.iloc[0]
-
-        strategy, stop=adv_stop_loss_strategy(price_series=prices_test_data)
-
-        backtest_df['strategy'] = strategy
-
-        #
-        backtest_df['stop'] = stop
-        backtest_df['stop_+'] = stop*(1+pct_gap)
-        backtest_df['stop_-'] = stop*(1-pct_gap)
-
-        # fig, ax = plt.subplots(2,1, sharex=True)
-        # backtest_df[['strategy', 'hold']].plot(ax=ax[0])
-        # prices_test_data.plot(ax=ax[1])
-        # backtest_df['stop'].plot(ax=ax[1], style='k')
-        # backtest_df['stop_+'].plot(ax=ax[1], style='g')
-        # backtest_df['stop_-'].plot(ax=ax[1], style='r')
-
-        for t in ['hold', 'strategy']:
-            compare_dic[t].append(backtest_df[t].iloc[-1]-100.0)
-
-        compare_dic['diff'].append(backtest_df['strategy'].iloc[-1]-backtest_df['hold'].iloc[-1])
-
-    return pd.DataFrame(data=compare_dic, index=date_list)
-
 
 if __name__=='__main__':
 
     mkt=mkt_data.MarketData()
 
-    price_data = mkt.crypto_data['XLM'].dropna()
-    df = back_test_random(price_data)
-    df.boxplot()
+    price_data = mkt.crypto_data['BTC'].loc[datetime.datetime(2017,11,1):].dropna()
 
-    for c in ['hold', 'strategy']:
-        sharpe = df[c].mean()/df[c].std()
-        print(c, sharpe)
+    backtest_df = pd.DataFrame()
+    backtest_df['price'] = price_data
+    backtest_df['hold'] = price_data*100.0/price_data.iloc[0]
+
+    strategy_result, stop=adv_stop_loss_strategy(price_series=price_data)
+
+    backtest_df['strategy'] = strategy_result
+
+
+    backtest_df['strategy'] = strategy_result
+
+    #
+    backtest_df['stop'] = stop
+    backtest_df['stop_+'] = stop*(1+pct_gap)
+    backtest_df['stop_-'] = stop*(1-pct_gap)
+
+    fig, ax = plt.subplots(2,1, sharex=True)
+    backtest_df[['strategy', 'hold']].plot(ax=ax[0])
+    price_data.plot(ax=ax[1])
+    backtest_df['stop'].plot(ax=ax[1], style='k')
+    backtest_df['stop_+'].plot(ax=ax[1], style='g')
+    backtest_df['stop_-'].plot(ax=ax[1], style='r')
 
     plt.show()
-    # df = pd.DataFrame()
-    # df['price'] = price_data
-    # df['hold'] = price_data*100.0/price_data.iloc[0]
-
-    # strategy=stop_loss_strategy(price_series=price_data)
-
-    # df['strategy'] = strategy
-
-    # stoploss_value = price_data.iloc[0]
-    # print(df.index[0])
-
-    # fig, ax = plt.subplots(2,1, sharex=True)
-    # df[['strategy', 'hold']].plot(ax=ax[0])
-    # df[['price']].plot(ax=ax[1])
-    # ax[1].plot([df.index[0],df.index[-1]],[stoploss_value,stoploss_value], 'k--')
-    # ax[1].plot([price_data.index[0],price_data.index[-1]],[stoploss_value*(1+0.01),stoploss_value*(1+0.01)], 'g--')
-    # ax[1].plot([price_data.index[0],price_data.index[-1]],[stoploss_value*(1-0.01),stoploss_value*(1-0.01)], 'r--')
-
-    # # print(df)
-    # plt.show()
