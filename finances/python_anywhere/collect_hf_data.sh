80e952a03c509fc919858e1031cc0fbfd5b0756f
pedrod_pythonanywhere <pedroadamas@gmail.com>
2018-03-12 23:55:55 +0000

    update data

--- finances/python_anywhere/collect_hf_data.sh
+++ finances/python_anywhere/collect_hf_data.sh
@@ -2,4 +2,4 @@ cd /home/hordeus/finances/finances/
 
 git checkout update_data
 
-/home/hordeus/.virtualenvs/finances/bin/python3.6 /home/hordeus/finances/python_anywhere/collect_trading_data.py
+/home/hordeus/.virtualenvs/finances/bin/python3.6 /home/hordeus/finances/finances/trading/bitstamp_api/collect_bitstamp_trading_data_PA.py
