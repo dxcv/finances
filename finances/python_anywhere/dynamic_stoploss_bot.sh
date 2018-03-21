df3d2a72a7dfb8450ad1ad631a09dcc2a9e7f85d
pedrod_pythonanywhere <pedroadamas@gmail.com>
2018-03-21 21:52:52 +0000

    update data and added batch file to run bot

---
+++ finances/python_anywhere/dynamic_stoploss_bot.sh
@@ -0,0 +1,5 @@
+cd /home/hordeus/finances/finances/
+
+git checkout update_data -q
+
+/home/hordeus/.virtualenvs/finances/bin/python3.6 /home/hordeus/finances/finances/trading/bots/run_dynamic_stoploss_bot.py
