acf0d5fc942d80f78f6062bddfae9c959bde29ac
Pedro <pedroadamas@gmail.com>
2018-03-23 22:18:33 +0100

    created specific files for each coin

---
+++ finances/trading/bots/dynamic_stoploss/run_dynamic_stoploss_bot_ltc.py
@@ -0,0 +1,24 @@
+import json
+
+from strategy_dynamic_stoploss import dynamic_stoploss_strategy
+
+import bitstamp.client as bts
+import os
+
+cfd, cfn = os.path.split(os.path.abspath(__file__))
+
+trading_client = bts.Trading(
+   username='769101',
+   key='9JShgcZgw3rlDvcCGVh4mi9QodcPZy82',
+   secret='GRKx4bOkKhDJh4Xex8eC3DtFK3MJwaO1'
+   )
+
+dynamic_stoploss_strategy(
+trading_client,
+coin='ltc',
+bot_status_json_path=os.path.join(cfd, 'trade_bot_status_ltc.json'),
+current_price=float(trading_client.ticker(base='ltc', quote='eur')['last']),
+pct_gap=0.035,
+minimum_gain=0.0475,
+reinvest_gap=0.35
+)
