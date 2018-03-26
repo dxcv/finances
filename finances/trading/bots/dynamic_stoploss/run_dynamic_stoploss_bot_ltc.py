a660422a32aa8f466c677097510bcc50183fdedb
Pedro <pedroadamas@gmail.com>
2018-03-26 23:13:40 +0200

    added ltc analysis and bot definition

--- finances/trading/bots/dynamic_stoploss/run_dynamic_stoploss_bot_ltc.py
+++ finances/trading/bots/dynamic_stoploss/run_dynamic_stoploss_bot_ltc.py
@@ -9,16 +9,17 @@ cfd, cfn = os.path.split(os.path.abspath(__file__))
 
 trading_client = bts.Trading(
    username='769101',
-   key='9JShgcZgw3rlDvcCGVh4mi9QodcPZy82',
-   secret='GRKx4bOkKhDJh4Xex8eC3DtFK3MJwaO1'
+   key='1uy9vw6puPcy6tO7L1qs2EdSZGaPeROU',
+   secret='5ofFS0z0wgUz44HqRXwnLDVCmzOEeBc1'
    )
 
+# to be run every 4 hours
 dynamic_stoploss_strategy(
 trading_client,
 coin='ltc',
 bot_status_json_path=os.path.join(cfd, 'trade_bot_status_ltc.json'),
 current_price=float(trading_client.ticker(base='ltc', quote='eur')['last']),
-pct_gap=0.035,
-minimum_gain=0.0475,
+pct_gap=0.025,
+minimum_gain=0.035,
 reinvest_gap=0.35
 )
