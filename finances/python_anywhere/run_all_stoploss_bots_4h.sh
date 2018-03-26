bee96d89629e6df84130ed787dcf3d6e8f6b4612
Pedro <pedroadamas@gmail.com>
2018-03-26 23:19:52 +0200

    added bash script for python anywhere

---
+++ finances/python_anywhere/run_all_stoploss_bots_4h.sh
@@ -0,0 +1,9 @@
+cd /home/hordeus/finances/finances/
+
+git checkout update_data -q
+
+# /home/hordeus/.virtualenvs/finances/bin/python3.6 /home/hordeus/finances/finances/trading/bots/dynamic_stoploss/run_dynamic_stoploss_bot_btc.py
+/home/hordeus/.virtualenvs/finances/bin/python3.6 /home/hordeus/finances/finances/trading/bots/dynamic_stoploss/run_dynamic_stoploss_bot_eth.py
+/home/hordeus/.virtualenvs/finances/bin/python3.6 /home/hordeus/finances/finances/trading/bots/dynamic_stoploss/run_dynamic_stoploss_bot_ltc.py
+# /home/hordeus/.virtualenvs/finances/bin/python3.6 /home/hordeus/finances/finances/trading/bots/dynamic_stoploss/run_dynamic_stoploss_bot_xrp.py
+# /home/hordeus/.virtualenvs/finances/bin/python3.6 /home/hordeus/finances/finances/trading/bots/dynamic_stoploss/run_dynamic_stoploss_bot_bch.py
--- finances/python_anywhere/test_bot_script.sh
+++
@@ -1,5 +0,0 @@
-cd /home/hordeus/finances/finances/
-
-git checkout update_data
-
-/home/hordeus/.virtualenvs/finances/bin/python3.6 /home/hordeus/finances/finances/trading/bitstamp_api/run_trading_public.py
