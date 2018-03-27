8c2f8833ef05387bc6a09a7eb5f668ab363b0a85
pedrod_pythonanywhere <pedroadamas@gmail.com>
2018-03-27 05:55:56 +0000

    corrections to code

--- finances/trading/bots/dynamic_stoploss/run_dynamic_stoploss_bot_ltc.py
+++ finances/trading/bots/dynamic_stoploss/run_dynamic_stoploss_bot_ltc.py
@@ -7,7 +7,7 @@ import os
 
 cfd, cfn = os.path.split(os.path.abspath(__file__))
 
-trading_client = bts.Trading(
+trading_client_ltc = bts.Trading(
    username='769101',
    key='1uy9vw6puPcy6tO7L1qs2EdSZGaPeROU',
    secret='5ofFS0z0wgUz44HqRXwnLDVCmzOEeBc1'
@@ -15,10 +15,10 @@ trading_client = bts.Trading(
 
 # to be run every 4 hours
 dynamic_stoploss_strategy(
-trading_client,
+trading_client_ltc,
 coin='ltc',
 bot_status_json_path=os.path.join(cfd, 'trade_bot_status_ltc.json'),
-current_price=float(trading_client.ticker(base='ltc', quote='eur')['last']),
+current_price=float(trading_client_ltc.ticker(base='ltc', quote='eur')['last']),
 pct_gap=0.025,
 minimum_gain=0.035,
 reinvest_gap=0.35
