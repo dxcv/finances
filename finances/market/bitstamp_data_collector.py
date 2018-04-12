da7346c6ff516876d7c24ef268e9acd5e7fa1068
Pedro <pedroadamas@gmail.com>
2018-04-12 23:55:27 +0200

    cleaned up directory

--- finances/market/bitstamp_data_collector.py
+++ finances/market/bitstamp_data_collector.py
@@ -9,8 +9,14 @@ import bitstamp.client as bts
 import time
 import pandas as pd
 import datetime
+import numpy as np
+
 
 import os
+
+cfd = os.path.dirname(os.path.realpath(__file__))
+
+exchanges_data_path = os.path.join(cfd, 'data_base', 'exchanges')
 
 def bitstamp_current_prices():
 
@@ -23,9 +29,10 @@ def bitstamp_current_prices():
 
     for coin_pair in list(current_prices.keys()):
         base=coin_pair.split('-')[0]
-        quote=coin_pair.split('-')[0]
+        quote=coin_pair.split('-')[1]
         try:
             current_prices[coin_pair] = float(bitstamp_public.ticker(base=base, quote=quote)['last'])
+            print('updated {}'.format(coin_pair))
         except:
             current_prices[coin_pair] = np.nan
             print('Coin pair {} raised error at time {}'.format(coin_pair, datetime.datetime.now()))
@@ -40,7 +47,8 @@ def create_bitstamp_data_chunk(n_values, step_in_mins):
     for k in range(n_values):
         times_list.append(datetime.datetime.now())
         prices_data.append(bitstamp_current_prices())
-        time.sleep(step_in_mins*60)
+        if k < n_values-1:
+            time.sleep(step_in_mins*60)
 
     data_chunk = pd.DataFrame(
         data=prices_data,
@@ -48,10 +56,10 @@ def create_bitstamp_data_chunk(n_values, step_in_mins):
 
     return data_chunk
 
-def update_bitstamp_data(
-    new_data,
-    bitstamp_csv_path=os.path.join(cfd, 'bitstamp_high_frequency_data.csv')
-    ):
+def update_bitstamp_data(new_data_chunk):
+
+    bitstamp_csv_path = os.path.join(exchanges_data_path, 'bitstamp_high_frequency_data.csv')
+    # if not os.path.exists(bitstamp_csv_path):
 
     prices_df = pd.read_csv(
         bitstamp_csv_path,
@@ -60,10 +68,12 @@ def update_bitstamp_data(
         infer_datetime_format=True
     )
 
-    prices_df.append(_new_data)
+    prices_df.append(new_data_chunk)
+    prices_df.to_csv(bitstamp_csv_path)
 
-    prices_df.to_csv(os.path.join(cfd, 'bitstamp_high_frequency_data.csv'))
     print('Data saved at {}'.format(datetime.datetime.now()))
 
-
 if __name__=='__main__':
+    bitstamp_csv_path = os.path.join(exchanges_data_path, 'bitstamp_high_frequency_data.csv')
+    a = create_bitstamp_data_chunk(2, 0.5)
+    a.to_csv(bitstamp_csv_path)
