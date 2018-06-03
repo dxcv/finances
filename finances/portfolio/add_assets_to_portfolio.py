1503b1bf1be159d4305e0f34c85031c7d876b886
Pedro <pedroadamas@gmail.com>
2018-06-03 20:39:54 +0200

    updated portfolio data

--- finances/portfolio/add_assets_to_portfolio.py
+++ finances/portfolio/add_assets_to_portfolio.py
@@ -10,23 +10,25 @@ from finances.portfolio.portfolio import PortFolio
 if __name__=='__main__':
 
     new_portfolio_assets = {
-        'BTC': 0.11307681,
-        'ETH': 1.23513731,
-        'LTC': 3.37977291,
-        'XRP': 757.544071,
-        'DASH': 0.146593,
-        'XMR': 0.59067,
-        'IOTA': 47.553,
-        'ADA': 217.639,
-        'XLM': 140.07,
-        'TRX': 0.237,
-        'BCH': 0.305129,
+        'BTC': 0.08060001625,
+        'ETH': 1.07980135,
+        'LTC': 3.03879891,
+        'XRP': 627.1118061,
+        'DASH': 0.167572,
+        'XMR': 0.722538,
+        'IOTA': 136.464,
+        'ADA': 552.304,
+        'XLM': 467.742,
+        'TRX': 1405.83,
+        'BCH': 0.303937,
         'FUN': 0.447,
         'EMC2': 45,
         'UBQ': 18.22222222,
         'BIS': 36.59233533,
         'ADST': 136.71,
-        'NEO': 1.297627,
+        'NEO': 1.577347,
+        'EOS': 7.992,
+        'ONT': 7.4076627,
     }
 
     import pylab as plt
@@ -40,7 +42,7 @@ if __name__=='__main__':
         # assets_prices = assets_effective_price
         )
 
-    myportfolio.insert_assets_at_date(assets=new_portfolio_assets, date=datetime.datetime(2018,4,3))
+    myportfolio.insert_assets_at_date(assets=new_portfolio_assets, date=datetime.datetime(2018,6,3))
     myportfolio.update_data()
     myportfolio.save_data()
     
