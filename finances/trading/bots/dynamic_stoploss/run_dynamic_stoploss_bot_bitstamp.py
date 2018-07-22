ccce7f12cab6ba8b26a1d3f325514160606d002e
pedrod_pythonanywhere <pedroadamas@gmail.com>
2018-07-22 12:12:35 +0000

    added try to the bots thing. To check again

--- finances/trading/bots/dynamic_stoploss/run_dynamic_stoploss_bot_bitstamp.py
+++ finances/trading/bots/dynamic_stoploss/run_dynamic_stoploss_bot_bitstamp.py
@@ -2,6 +2,7 @@ from strategy_dynamic_stoploss import dynamic_stoploss_bitstamp_bot
 
 import bitstamp.client as bts
 import os
+import time
 
 cfd, cfn = os.path.split(os.path.abspath(__file__))
 
@@ -11,62 +12,74 @@ trading_client = bts.Trading(
    secret='u4eBOyegwk5VJLHqwlEaOjULUebOT9Yk'
    )
 
-print('BTC bot:')
-# BITCOIN
-dynamic_stoploss_bitstamp_bot(
-    trading_client,
-    coin='btc',
-    bot_status_json_path=os.path.join(cfd, 'trade_bot_status_bitstamp.json'),
-    current_price=float(trading_client.ticker(base='btc', quote='eur')['last']),
-    pct_gap=0.03,
-    minimum_gain=0.01,
-    reinvest_gap=0.5
-    )
+failed = True
+failed_times = 0
 
-print('ETH bot:')
-# ETHEREUM
-dynamic_stoploss_bitstamp_bot(
-    trading_client,
-    coin='eth',
-    bot_status_json_path=os.path.join(cfd, 'trade_bot_status_bitstamp.json'),
-    current_price=float(trading_client.ticker(base='eth', quote='eur')['last']),
-    pct_gap=0.03,
-    minimum_gain=0.03,
-    reinvest_gap=0.5
-    )
+while failed and failed_times<3:
+    try:
+        print('BTC bot:')
+        # BITCOIN
+        dynamic_stoploss_bitstamp_bot(
+            trading_client,
+            coin='btc',
+            bot_status_json_path=os.path.join(cfd, 'trade_bot_status_bitstamp.json'),
+            current_price=float(trading_client.ticker(base='btc', quote='eur')['last']),
+            pct_gap=0.03,
+            minimum_gain=0.01,
+            reinvest_gap=0.5
+            )
 
-print('LTC bot:')
-# LITECOIN
-dynamic_stoploss_bitstamp_bot(
-    trading_client,
-    coin='ltc',
-    bot_status_json_path=os.path.join(cfd, 'trade_bot_status_bitstamp.json'),
-    current_price=float(trading_client.ticker(base='ltc', quote='eur')['last']),
-    pct_gap=0.01,
-    minimum_gain=0.04,
-    reinvest_gap=0.5
-    )
+        print('ETH bot:')
+        # ETHEREUM
+        dynamic_stoploss_bitstamp_bot(
+            trading_client,
+            coin='eth',
+            bot_status_json_path=os.path.join(cfd, 'trade_bot_status_bitstamp.json'),
+            current_price=float(trading_client.ticker(base='eth', quote='eur')['last']),
+            pct_gap=0.03,
+            minimum_gain=0.03,
+            reinvest_gap=0.5
+            )
 
-print('XRP bot:')
-# RIPPLE
-dynamic_stoploss_bitstamp_bot(
-    trading_client,
-    coin='xrp',
-    bot_status_json_path=os.path.join(cfd, 'trade_bot_status_bitstamp.json'),
-    current_price=float(trading_client.ticker(base='xrp', quote='eur')['last']),
-    pct_gap=0.05,
-    minimum_gain=0.02,
-    reinvest_gap=0.5
-    )
+        print('LTC bot:')
+        # LITECOIN
+        dynamic_stoploss_bitstamp_bot(
+            trading_client,
+            coin='ltc',
+            bot_status_json_path=os.path.join(cfd, 'trade_bot_status_bitstamp.json'),
+            current_price=float(trading_client.ticker(base='ltc', quote='eur')['last']),
+            pct_gap=0.01,
+            minimum_gain=0.04,
+            reinvest_gap=0.5
+            )
 
-print('BCH bot:')
-# BITCOIN CASH
-dynamic_stoploss_bitstamp_bot(
-    trading_client,
-    coin='bch',
-    bot_status_json_path=os.path.join(cfd, 'trade_bot_status_bitstamp.json'),
-    current_price=float(trading_client.ticker(base='bch', quote='eur')['last']),
-    pct_gap=0.05,
-    minimum_gain=0.08,
-    reinvest_gap=0.5
-    )
+        print('XRP bot:')
+        # RIPPLE
+        dynamic_stoploss_bitstamp_bot(
+            trading_client,
+            coin='xrp',
+            bot_status_json_path=os.path.join(cfd, 'trade_bot_status_bitstamp.json'),
+            current_price=float(trading_client.ticker(base='xrp', quote='eur')['last']),
+            pct_gap=0.05,
+            minimum_gain=0.02,
+            reinvest_gap=0.5
+            )
+
+        print('BCH bot:')
+        # BITCOIN CASH
+        dynamic_stoploss_bitstamp_bot(
+            trading_client,
+            coin='bch',
+            bot_status_json_path=os.path.join(cfd, 'trade_bot_status_bitstamp.json'),
+            current_price=float(trading_client.ticker(base='bch', quote='eur')['last']),
+            pct_gap=0.05,
+            minimum_gain=0.08,
+            reinvest_gap=0.5
+            )
+
+        failed = False
+
+    except bts.BitstampError:
+        failed_times +=1
+        print('Failed... repeating!')
+        time.sleep(10)
