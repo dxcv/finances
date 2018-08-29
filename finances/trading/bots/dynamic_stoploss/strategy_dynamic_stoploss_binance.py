fa31553941cd6ece906b023043a91fe8bd83d332
Pedro <pedroadamas@gmail.com>
2018-08-29 22:40:27 +0200

    WORKING!

--- finances/trading/bots/dynamic_stoploss/strategy_dynamic_stoploss_binance.py
+++ finances/trading/bots/dynamic_stoploss/strategy_dynamic_stoploss_binance.py
@@ -2,6 +2,7 @@ import json
 import time
 
 from finances.trading.strategies.dynamic_stoploss.dynamic_stoploss_strategy import dynamic_stoploss_strategy
+from binance.exceptions import BinanceAPIException
 
 def buy_all(trading_client, coin, usd_quantity):
 
@@ -11,49 +12,54 @@ def buy_all(trading_client, coin, usd_quantity):
         price_list[pair['symbol']] = float(pair['price'])
 
     # extract the relevant prices
-    current_btc_price = price_list['BTCUSD']
+    current_btc_price = price_list['BTCUSDT']
     current_price_in_btc= price_list[coin+'BTC']
     current_price_in_usd = (current_price_in_btc*current_btc_price)
 
     # calculate the amount of both coin and btc to buy
-    amount_coin_to_buy = usd_quantity/current_price_in_usd
-    amount_btc_to_buy = usd_quantity/current_price_in_btc
+    amount_to_buy = usd_quantity/current_price_in_usd
+    btc_to_buy = usd_quantity/current_btc_price
 
     bought_btc=False
     bought_coin=False
 
     # first buy the corresponding amount of btc
-    while not bought_btc and amount_btc_to_buy>0:
+    while not bought_btc and btc_to_buy>0:
         try:
             trading_client.order_market_buy(
-                symbol='BTCUSD',
-                quantity=int(amount_btc_to_buy)  # <--------------- this is the thing that needs to be changed
+                symbol='BTCUSDT',
+                quantity=round(btc_to_buy, 5)
             )
             bought_btc=True
-            print('Bought {0} BTC'.format(amount_btc_to_buy))
-        except:
-            amount_btc_to_buy=0.9975*amount_btc_to_buy
+            print('Bought {0} BTC'.format(btc_to_buy))
+        except BinanceAPIException as e:
+            btc_to_buy*=0.9975
 
     # then buy the corresponding amount of the required coin
-    time.sleep(10)  # wait a bit for the previous transaction take place
-    while not bought_coin and amount_coin_to_buy>0:
+    time.sleep(3)  # wait a bit for the previous transaction take place
+    rounder=5
+    
+    while not bought_coin and amount_to_buy>0.0:
         try:
             trading_client.order_market_buy(
                 symbol=coin+'BTC',
-                quantity=int(amount_coin_to_buy) # <--------------- this is the thing that needs to be changed
+                quantity=round(amount_to_buy, rounder)
             )
             bought_coin=True
-            print('Bought {0} {1}'.format(amount_coin_to_buy, coin))
-        except:
-            amount_coin_to_buy=0.9975*amount_coin_to_buy
+            print('Bought {0} {1}'.format(amount_to_buy, coin))
+        except BinanceAPIException as e:
+            if 'LOT_SIZE' in str(e):
+                rounder -=1
+            if 'insufficient balance' in str(e):
+                amount_to_buy*=0.9975
 
 
 def sell_all(trading_client, coin):
     coin_available = float(trading_client.get_asset_balance(asset=coin)['free'])
     amount_to_sell = coin_available
-    print(amount_to_sell)
 
     sold_coin=False
+    rounder = 5
     sold_btc=False
 
     # first sell all to btc
@@ -61,27 +67,30 @@ def sell_all(trading_client, coin):
         try:
             trading_client.order_market_sell(
                 symbol=coin+'BTC',
-                quantity=amount_to_sell
+                quantity=round(amount_to_sell, rounder)
             )
             sold_coin=True
             print('Sold {0} {1}'.format(amount_to_sell, coin))
-        except:
-            amount_to_sell=amount_to_sell*0.9975
+        except BinanceAPIException as e:
+            if 'LOT_SIZE' in str(e):
+                rounder -=1
+            if 'insufficient balance' in str(e):
+                amount_to_sell*=0.9975
 
     # then sell all btc to usd
-    time.sleep(10)  # wait a bit for the previous transaction take place
+    time.sleep(3)  # wait a bit for the previous transaction take place
     
     btc_available = float(trading_client.get_asset_balance(asset='btc')['free'])
     btc_to_sell = btc_available
     while not sold_btc and btc_to_sell>0:
         try:
             trading_client.order_market_sell(
-                symbol='BTCUSD',
-                quantity=btc_to_sell
+                symbol='BTCUSDT',
+                quantity=round(btc_to_sell, 5)
             )
-            sold_coin=True
+            sold_btc=True
             print('Sold {0} BTC'.format(btc_to_sell))
-        except:
+        except BinanceAPIException as e:
             btc_to_sell=btc_to_sell*0.9975
 
     return amount_to_sell
