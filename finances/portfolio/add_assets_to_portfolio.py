c8a992cd4aea7ce3bc5e005827161cf4fba5f22c
Pedro <pedroadamas@gmail.com>
2018-08-29 15:54:17 +0200

    updated portfolio

--- finances/portfolio/add_assets_to_portfolio.py
+++ finances/portfolio/add_assets_to_portfolio.py
@@ -10,17 +10,17 @@ from finances.portfolio.portfolio import PortFolio
 if __name__=='__main__':
 
     new_portfolio_assets = {
-        'BTC': 0.08060001625,
-        'ETH': 1.07980135,
-        'LTC': 3.03879891,
-        'XRP': 627.1118061,
+        'BTC': 0.09475401625,
+        'ETH': 1.28753416,
+        'LTC': 5.53657191,
+        'XRP': 769.397143,
         'DASH': 0.167572,
         'XMR': 0.722538,
         'IOTA': 136.464,
         'ADA': 552.304,
         'XLM': 467.742,
         'TRX': 1405.83,
-        'BCH': 0.303937,
+        'BCH': 0.321836,
         'FUN': 0.447,
         'EMC2': 45,
         'UBQ': 18.22222222,
@@ -42,7 +42,7 @@ if __name__=='__main__':
         # assets_prices = assets_effective_price
         )
 
-    myportfolio.insert_assets_at_date(assets=new_portfolio_assets, date=datetime.datetime(2018,6,3))
+    myportfolio.insert_assets_at_date(assets=new_portfolio_assets, date=datetime.datetime(2018,8,24))
     myportfolio.update_data()
     myportfolio.save_data()
     
