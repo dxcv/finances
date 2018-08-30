1dcd02865bf5128783fa40b1b77453b4670115b6
Pedro <pedroadamas@gmail.com>
2018-08-30 23:03:31 +0200

    added for the python anywhere running file

--- finances/python_anywhere/run_all_stoploss_bots_12h.sh
+++ finances/python_anywhere/run_all_stoploss_bots_12h.sh
@@ -2,5 +2,8 @@ cd /home/hordeus/finances/finances/
 
 git checkout update_data -q
 
-echo "Running bots:"
+echo "Running bots bitstamp:"
 /home/hordeus/.virtualenvs/finances/bin/python3.6 /home/hordeus/finances/finances/trading/bots/dynamic_stoploss/run_dynamic_stoploss_bot_bitstamp.py
+
+echo "Running bots binance:"
+/home/hordeus/.virtualenvs/finances/bin/python3.6 /home/hordeus/finances/finances/trading/bots/dynamic_stoploss/run_dynamic_stoploss_bot_binance.py
