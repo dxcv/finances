32b04cb08a701af5fb706db93b2a0fc655988478
Pedro <pedroadamas@gmail.com>
2019-03-17 16:27:08 +0100

    first example of code plotting the portfolio composition

--- finances/investment/optimization.py
+++ finances/investment/optimization.py
@@ -26,17 +26,6 @@ def get_optimized_allocation(cov_mat, exp_vect, target_mean, budget, current_pri
 
     n = len(cov_mat)
 
-
-    # # print(cov_mat.values)
-    # GG = np.vstack(
-    #         (-exp_vect.values,
-    #         -np.identity(n),
-    #         # current_prices.values
-    #         ))
-    # print(GG)
-
-    # exit(0)
-
     P = opt.matrix(cov_mat.values)
     q = opt.matrix(0.0, (n, 1))
 
@@ -75,62 +64,75 @@ def get_optimized_allocation(cov_mat, exp_vect, target_mean, budget, current_pri
 
     return alpha
 
+def create_efficient_frontier_dataset(exp_hor, cov_hori, current_prices, budget=5000):
+    print(current_prices)
+    efficient_frontier = pd.DataFrame()
+    maximum_expt=max((exp_hor/current_prices).values*budget)
+    for m in np.arange(budget,maximum_expt,50):
+        try:
+            alpha = get_optimized_allocation(
+                cov_mat=cov_hori,
+                exp_vect=exp_hor,
+                target_mean=m,
+                budget=budget,
+                current_prices=current_prices)
+        except ValueError:
+            print('ERROR', m)
+            break
+        efficient_frontier[m] = alpha#*current_prices/sum(alpha*current_prices)
+    return efficient_frontier
 
 
-from finances.investment.multivariate_estimation import shrinked_estimate_multivariate
-os.environ['TIINGO_API_KEY'] = 'ba62a0fba810f937382b5e772f8f152b58c4ebfc'
+def calculate_std(x):
+    x1,x2 = np.meshgrid(x, x)
+    std = np.sqrt(np.sum(np.sum(x1*cov_hori*x2)))
+    return std
 
-N_DAYS = 15
-
-# Load data from statsmodels datasets
-start = datetime(2014, 9, 1)
-end = datetime(2019, 9, 1)
-df = web.DataReader([
-    'AMZN','GOOGL','SPY','AMT',
-    'AAPL','MSFT','MMM','VOO',
-    'ABMD','ABBV','BA'
-    ], 'tiingo', start, end)
-df['logP'] = np.log(df['close'])
-df['cum_rets'] = df['logP'].rolling(2).apply(lambda x: x[-1]-x[0], raw=True).dropna()
-data = df['cum_rets'].unstack('symbol').dropna()
+# def plot_porfolio_distribution(efficient_frontier):
 
 
-shrinked_mean, shrinked_cov = shrinked_estimate_multivariate(data)
+if __name__ == '__main__':
+    from finances.investment.multivariate_estimation import shrinked_estimate_multivariate
 
-current_prices = df['close'].unstack('symbol').dropna().iloc[-1]
+    import seaborn as sns
 
+    np.random.seed(seed=233423)
 
-exp_hor, cov_hori = prices_at_horizon(current_prices,mean=shrinked_mean,cov=shrinked_cov,n_periods=120)
+    sns.set_style('whitegrid')
+    sns.set_context('talk')
+    sns.set_palette('dark')
 
+    # os.environ['TIINGO_API_KEY'] = 'ba62a0fba810f937382b5e772f8f152b58c4ebfc'
 
-expt_list = []
-std_list = []
-for e in np.arange(1000,1300,10):
-    alpha = get_optimized_allocation(
-        cov_mat=cov_hori,
-        exp_vect=exp_hor,
-        target_mean=e,
-        budget=1000.0,
-        current_prices=current_prices)
-
-    print(alpha)
-
-    expected_val = np.sum(alpha.values*exp_hor)
-
-    a1,a2 = np.meshgrid(alpha, alpha)
-    std = np.sqrt(np.sum(np.sum(a1*cov_hori*a2)))
-
-    expt_list.append(expected_val)
-    std_list.append(std)
+    # # Load data from statsmodels datasets
+    # start = datetime(2014, 9, 1)
+    # end = datetime(2019, 9, 1)
+    # df = web.DataReader([
+    #     'AMZN','GOOGL','AMT',
+    #     'AAPL','MSFT','MMM','VOO',
+    #     'ABMD','ABBV','V', 'BA'
+    #     ], 'tiingo', start, end)
+    # df['logP'] = np.log(df['close'])
+    # df['cum_rets'] = df['logP'].rolling(2).apply(lambda x: x[-1]-x[0], raw=True).dropna()
+    # df.to_csv('data_test.csv')
 
 
-import seaborn as sns
+    df = pd.read_csv('data_test.csv', index_col=['symbol', 'date'])
 
-np.random.seed(seed=233423)
+    data = df['cum_rets'].unstack('symbol').dropna()
 
-sns.set_style('whitegrid')
-sns.set_context('talk')
-sns.set_palette('dark')
+    shrinked_mean, shrinked_cov = shrinked_estimate_multivariate(data)
 
-plt.plot(std_list, expt_list)
-plt.show()
+    current_prices = df['close'].unstack('symbol').dropna().iloc[-1]
+
+    exp_hor, cov_hori = prices_at_horizon(current_prices,mean=shrinked_mean,cov=shrinked_cov,n_periods=120)
+
+    fig, ax = plt.subplots(2,1, sharex=True)
+
+    frontier = create_efficient_frontier_dataset(exp_hor, cov_hori, current_prices)
+    frontier.apply(calculate_std).plot(style='-o', ax=ax[0])
+    weigths = frontier.apply(lambda x: x*current_prices/sum(x*current_prices))
+    print(weigths)
+    a = weigths.rolling(len(weigths), min_periods=1).sum()
+    a.transpose().plot(ax=ax[1])
+    plt.show()
