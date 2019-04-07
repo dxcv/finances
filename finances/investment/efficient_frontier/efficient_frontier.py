c12bbfe5d9853c4ba675ecda823e1ff35a9c94e9
Pedro <pedroadamas@gmail.com>
2019-04-07 15:25:27 +0200

    edited to include all the functions used locally. Included the right function for the scatter!

--- finances/investment/efficient_frontier/efficient_frontier.py
+++ finances/investment/efficient_frontier/efficient_frontier.py
@@ -30,16 +30,85 @@ def volatility(weights, cov_matrix, gamma=0):
     """
     L2_reg = gamma * (weights ** 2).sum()
     portfolio_volatility = np.dot(weights.T, np.dot(cov_matrix, weights))
-    return portfolio_volatility + L2_reg
+    return np.sqrt(portfolio_volatility + L2_reg)
 
 
 class EfficientFrontier(OriginalEF):
     mv_frontier = None
 
+    def min_volatility(self):
+        """
+        Minimise volatility.
+
+        :return: asset weights for the volatility-minimising portfolio
+        :rtype: dict
+        """
+        args = (self.cov_matrix, self.gamma)
+        result = sco.minimize(
+            volatility,
+            x0=self.initial_guess,
+            args=args,
+            method="SLSQP",
+            bounds=self.bounds,
+            constraints=self.constraints,
+        )
+        self.weights = result["x"]
+        return dict(zip(self.tickers, self.weights))
+
+
+    def efficient_return(self, target_return, market_neutral=False):
+        """
+        Calculate the 'Markowitz portfolio', minimising volatility for a given target return.
+
+        :param target_return: the desired return of the resulting portfolio.
+        :type target_return: float
+        :param market_neutral: whether the portfolio should be market neutral (weights sum to zero),
+                               defaults to False. Requires negative lower weight bound.
+        :type market_neutral: bool, optional
+        :raises ValueError: if ``target_return`` is not a positive float
+        :return: asset weights for the Markowitz portfolio
+        :rtype: dict
+        """
+        if not isinstance(target_return, float) or target_return < 0:
+            raise ValueError("target_risk should be a positive float")
+
+        args = (self.cov_matrix, self.gamma)
+        target_constraint = {
+            "type": "eq",
+            "fun": lambda w: w.dot(self.expected_returns) - target_return,
+        }
+        # The equality constraint is either "weights sum to 1" (default), or
+        # "weights sum to 0" (market neutral).
+        if market_neutral:
+            if self.bounds[0][0] is not None and self.bounds[0][0] >= 0:
+                warnings.warn(
+                    "Market neutrality requires shorting - bounds have been amended",
+                    RuntimeWarning,
+                )
+                self.bounds = self._make_valid_bounds((-1, 1))
+            constraints = [
+                {"type": "eq", "fun": lambda x: np.sum(x)},
+                target_constraint,
+            ]
+        else:
+            constraints = self.constraints + [target_constraint]
+
+        result = sco.minimize(
+            volatility,
+            x0=self.initial_guess,
+            args=args,
+            method="SLSQP",
+            bounds=self.bounds,
+            constraints=constraints,
+        )
+        self.weights = result["x"]
+        return dict(zip(self.tickers, self.weights))
+
     def create_mv_frontier(self, n_points):
 
-        min_vol_weigths = self.min_volatility()
-        min_er = max(np.sum(self.weights*self.expected_returns.values),0)
+        min_vol_weigths = pd.Series(self.min_volatility())
+        min_er = max(np.sum(min_vol_weigths*self.expected_returns.values),0)
+        print(volatility(min_vol_weigths, self.cov_matrix, gamma=0))
 
         max_er = max(self.expected_returns)
         er_list = np.linspace(min_er, max_er, n_points)
@@ -77,7 +146,7 @@ class EfficientFrontier(OriginalEF):
         df = self.efficient_frontier
         df = df.set_index('volatility')
         
-        df['expected_returns'].plot(style='-o', ax=ax[0])
+        df['expected_returns'].plot(style='r-', ax=ax[0])
 
         weights = df.drop('expected_returns', axis=1).T
         to_plot = weights.rolling(len(weights), min_periods=1).sum().T
@@ -129,12 +198,37 @@ class EfficientFrontier(OriginalEF):
         return df
 
     def plot_scatter_efficient(self, num_portfolios):
-        fig, ax = plt.subplots(1,1, sharex=True)
-        x = self.mv_frontier['volatility']
-        y = self.mv_frontier['expected_returns']
-        
-        ax.plot(x,y, 'k--')
+        fig = plt.figure()
 
-        df_monte_carlo = self.create_portfolio_scatter(num_portfolios)
-        df_monte_carlo.plot.scatter(x='Volatility', y='Returns', ax=ax)
-        return ax
+        df = self.efficient_frontier
+
+        # generate the random portfolios
+        np.random.seed(42)
+        n_assets = len(self.tickers)
+        all_weights = np.zeros((num_portfolios, n_assets))
+        ret_arr = np.zeros(num_portfolios)
+        vol_arr = np.zeros(num_portfolios)
+        sharpe_arr = np.zeros(num_portfolios)
+
+        for x in range(num_portfolios):
+            # Weights
+            weights = np.array(np.random.random(n_assets))
+            weights = weights/np.sum(weights)
+            
+            # Save weights
+            all_weights[x,:] = weights
+            
+            # Expected return
+            ret_arr[x] = np.sum( (self.expected_returns * weights ))
+            
+            # Expected volatility
+            vol_arr[x] = volatility(weights, self.cov_matrix)
+            
+            # Sharpe Ratio
+            sharpe_arr[x] = ret_arr[x]/vol_arr[x]
+
+
+        ax = df.plot(x='volatility', y='expected_returns', style='k-')
+
+        ax.scatter(vol_arr, ret_arr, c=sharpe_arr, cmap='viridis')
+        return fig
